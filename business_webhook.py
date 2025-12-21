9096ef5d34e961d23f4e5da498408da6afac06a2
m88399814-ctrl <m88399814@gmail.com>
2025-12-21 16:23:14 +0300

    Update business_webhook.py

--- business_webhook.py
+++ business_webhook.py
@@ -789,6 +789,7 @@ def trial_expired_text(start_date: str, end_date: str, ref_link: str):
         "если 2 твоих друга с Telegram Premium запустят и подключат бота по твоей ссылке:\n"
         f"<blockquote><code>{ref_link}</code></blockquote>\n\n"
         "<b>Ну, или продлить платно (см. ниже)</b>\n"
+        "<blockquote>Если вдруг хотите оплатить другой любой валютой, пишите админу /support</blockquote>"
         "<b>Вопросы?</b> — /support"
     )
 
